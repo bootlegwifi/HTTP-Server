18a965c3faedc5d463187daabdc66d2e1a4c19fa
madison <madisoncraig@protonmail.com>
2023-10-24 17:17:01 -0700

    cleanup

--- server.py
+++ server.py
@@ -10,22 +10,22 @@ from blessings import Terminal
 t = Terminal()
 
 def fastshell():
-	cwd = os.getcwd()
-	username = os.getlogin()
-	print("[" + t.green("+") + "][" + username + ":" + cwd + "]$") 
-	print("[" + t.green("+") + "]Enter 'Q' to quit")
-	try:
-		while True:
-			command = input("\n<" + t.cyan("SERVER") + ">$ ")
-			if not command in ('q', 'Q'):
-				os.system(command)
-			else:
-				print("\n[" + t.red("!") + "]Exiting shell")
-				time.sleep(1.5)
-				break
-	
-	except KeyboardInterrupt:
-		print("\n[" + t.red("!") + "]Critical. User Aborted")
+    cwd = os.getcwd()
+    username = os.getlogin()
+    print("[" + t.green("+") + "][" + username + ":" + cwd + "]$") 
+    print("[" + t.green("+") + "]Enter 'Q' to quit")
+    try:
+        while True:
+            command = input("\n<" + t.cyan("SERVER") + ">$ ")
+            if not command in ('q', 'Q'):
+                os.system(command)
+            else:
+                print("\n[" + t.red("!") + "]Exiting shell")
+                time.sleep(1.5)
+                break
+
+    except KeyboardInterrupt:
+        print("\n[" + t.red("!") + "]Critical. User Aborted")
 
 def invokeshell():
     print("[" + t.green("+") + "]Invoke a shell to make changes in server directory?")
@@ -46,16 +46,16 @@ if default == 'y':
     IP = "127.0.0.1"
     invokeshell()
     print("\n[" + t.green("+") + "]Default config loaded\n")
-	
+
 elif default == 'n':
-	
-	print("[" + t.green("+") + "]Specify values:\n")
-	PORT = eval(input("Port #: "))
-	IP = input("Host IP: ")
-	invokeshell()
-		
+
+    print("[" + t.green("+") + "]Specify values:\n")
+    PORT = eval(input("Port #: "))
+    IP = input("Host IP: ")
+    invokeshell()
+
 else:
-	print("\n[" + t.red("!") + "]Unhandled Option")
+    print("\n[" + t.red("!") + "]Unhandled Option")
 
 
 print("[" + t.green("+") + "]Starting Server...\n")
@@ -66,15 +66,15 @@ Handler.extensions_map.update({
 });
 
 try:
-	httpd = socketserver.TCPServer((IP, PORT), Handler)
+    httpd = socketserver.TCPServer((IP, PORT), Handler)
 except Exception as e:
-	print("\n[" + t.red("!") + "]Critical! An exception was raised with the following error message:")
-	print(e)
+    print("\n[" + t.red("!") + "]Critical! An exception was raised with the following error message:")
+    print(e)
 
 print("[" + t.green("+") + "]Serving at:", IP, repr(PORT))
 
 # Watching keyboard interrupt
 try:
-	httpd.serve_forever()
+    httpd.serve_forever()
 except KeyboardInterrupt:
-	print("\n[" + t.red("!") + "]User Aborted")
+    print("\n[" + t.red("!") + "]User Aborted")
